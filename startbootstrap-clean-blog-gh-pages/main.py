73784d39126642cc2562bfb87d9a0114aba67b73
NaduniTashana <nadutash0@gmail.com>
2024-06-17 11:31:00 -0700

    first

---
+++ startbootstrap-clean-blog-gh-pages/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+posts= requests.get("https://api.npoint.io/674f5423f73deab1e9a7").json()
+
+@app.route("/")
+def get_all_blogs():
+    return render_template("index.html", all_posts= posts)
+
+@app.route("/post/<int:num>")
+def get_blog(num):
+    post= None
+    for blog_post in posts:
+        if blog_post["id"]== int(num):
+            post= blog_post
+    return render_template("post.html", post=post)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
